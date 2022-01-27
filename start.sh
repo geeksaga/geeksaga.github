403d31d1d32ae9f1cc9d49ad147a15c56e92bbac
albert <albert@jennifersoft.com>
2022-01-27 17:04:03 +0900

    Hugo Test

--- start.sh
+++ start.sh
@@ -5,5 +5,4 @@ LC_ALL=en_US.UTF-8
 
 #jekyll serve --watch --drafts
 
-#bundle exec jekyll serve --trace --watch --drafts --unpublished
-bundle exec jekyll serve --host 192.168.9.134 --port 17908 --trace --watch --drafts --unpublished
+bundle exec jekyll serve --trace --watch --drafts --unpublished
